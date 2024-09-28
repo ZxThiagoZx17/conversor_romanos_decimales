f311d404a006a843327367390ba3c6c92b231a03
unknown <tiagosanabria03@gmail.com>
2024-09-28 17:39:52 -0500

    Se arregla error en la entrada de la funcion que convierte romanos a decimales

--- conversor/utils.py
+++ conversor/utils.py
@@ -13,6 +13,7 @@ class RomanNumerals:
     def from_roman(roman_num : str) -> int:
         valor_romano={"M":1000, "CM":900, "D":500,"CD":400,"C":100,"XC":90,"L":50,"XL":40,"X":10,"IX":9,"V":5,"IV":4,"I":1}
         total = 0
+        roman_num = roman_num.upper()
         lista_dec = [valor_romano.get(i) for i in roman_num]
         for ind , valor in enumerate(lista_dec):
             if ind+1 != len(lista_dec):
