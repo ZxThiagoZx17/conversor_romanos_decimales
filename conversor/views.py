3dc915b8e2354361240208c7e8ba519e8cc302ef
unknown <tiagosanabria03@gmail.com>
2024-09-28 16:34:13 -0500

    Creamos la vista correspondiente al conversor

--- conversor/views.py
+++ conversor/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .utils import RomanNumerals
 
 # Create your views here.
+def converter_view(request):
+    result_decimal = None
+    result_roman = None
+    
+    if request.method == 'POST':
+        roman_number = request.POST.get('roman_number', '')
+        decimal_number = request.POST.get('decimal_number', '')
+
+        # Convertir de romano a decimal
+        if roman_number:
+            result_decimal = RomanNumerals.from_roman(roman_number)
+
+        # Convertir de decimal a romano
+        if decimal_number:
+            result_roman = RomanNumerals.to_roman(int(decimal_number))
+
+    return render(request, 'conversor/converter.html', {
+        'result_decimal': result_decimal,
+        'result_roman': result_roman,
+    })
